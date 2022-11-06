370cbd5e6e7a346f3cd20d90deec46ea81751133
KotaShimomura <shimonarudo@gmail.com>
2022-11-07 01:24:15 +0900

    fix

--- run.sh
+++ run.sh
@@ -5,3 +5,9 @@ python -u main.py --drop_last y \
                 --loss_balance y \
                 --log_to_file n \
                 --distributed \
+                --max_iters 80000 \
+                --train_batch_size 8 \
+                --base_lr 0.01 \
+                --data_dir /workspace/workspace/ProtoSeg_local/data/Cityscapes \
+                --checkpoints_root /workspace/workspace/ProtoSeg_local/output/Cityscapes \
+                --pretrained /workspace/workspace/ProtoSeg_local/checkpoints/cityscapes/hrnetv2_w48_imagenet_pretrained.pth
